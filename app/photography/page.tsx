f22277aac4c1f7f97fddecd986fd0519bf7dcc59
tomzhengy <tomzheng1012@gmail.com>
2026-04-11 14:41:04 -0700

    feat: wire up mosaic grid and widen photography page to 1080px

--- app/photography/page.tsx
+++ app/photography/page.tsx
@@ -1,16 +1,23 @@
+import { Metadata } from "next";
 import Header from "../components/sections/Header";
 import SocialLinks from "../components/sections/SocialLinks";
 import LastVisitor from "../components/sections/LastVisitor";
 import ThemeToggle from "../components/ui/theme/ThemeToggle";
+import MosaicGrid from "../components/photography/MosaicGrid";
+
+export const metadata: Metadata = {
+  title: "Photography - Tom Zheng",
+  description: "Photography and motion work by Tom Zheng.",
+};
 
 export default function PhotographyPage() {
   return (
     <main className="flex min-h-screen justify-center">
-      <div className="text-left max-w-[540px] w-full px-4 pt-[8vh] pb-16">
+      <div className="text-left max-w-[1080px] w-full px-4 pt-[8vh] pb-16">
         <Header ThemeToggleComponent={ThemeToggle} currentPage="photography" />
 
         <section aria-labelledby="photography" className="mt-6">
-          <p className="text-lg">Coming soon.</p>
+          <MosaicGrid />
         </section>
 
         <div className="mt-8">
