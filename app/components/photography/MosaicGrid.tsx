8fe4ab86a8e5c0a3dbf7d3613903d198b467fb1c
tomzhengy <tomzheng1012@gmail.com>
2026-04-12 01:43:28 -0700

    style: widen description column from 200px to 240px

--- app/components/photography/MosaicGrid.tsx
+++ app/components/photography/MosaicGrid.tsx
@@ -247,7 +247,7 @@ export default function MosaicGrid({
   return (
     <div className="flex gap-8">
       {/* left: text panel */}
-      <div className="hidden md:block w-[200px] shrink-0 self-start sticky top-[8vh] h-[calc(100vh-8vh)] overflow-hidden break-words pl-1">
+      <div className="hidden md:block w-[240px] shrink-0 self-start sticky top-[8vh] h-[calc(100vh-8vh)] overflow-hidden break-words">
         {/* spacer to align with photos below header */}
         <div className="h-8 mb-8" />
         <div className="mt-6">
@@ -458,7 +458,7 @@ export default function MosaicGrid({
             >
               {/* left: text panel */}
               <div
-                className="hidden md:flex w-[200px] min-w-0 shrink-0 flex-col justify-center pl-4 break-words overflow-hidden"
+                className="hidden md:flex w-[240px] min-w-0 shrink-0 flex-col justify-center pl-4 break-words overflow-hidden"
                 onClick={(e) => e.stopPropagation()}
               >
                 {isDevMode ? (
