d87bdbabfd165f2b3573dafcaaf3981820c8757b
tomzhengy <tomzheng1012@gmail.com>
2026-04-12 02:20:05 -0700

    style: nudge description text down in sidebar and expanded view

--- app/components/photography/MosaicGrid.tsx
+++ app/components/photography/MosaicGrid.tsx
@@ -256,7 +256,7 @@ export default function MosaicGrid({
       <div className="hidden md:block w-[200px] shrink-0 self-start sticky top-[8vh] h-[calc(100vh-8vh)] overflow-hidden break-words">
         {/* spacer to align with photos below header */}
         <div className="h-8 mb-8" />
-        <div className="mt-6">
+        <div className="mt-6 pt-4">
           {displayedItem && (
             <>
               {isDevMode ? (
@@ -466,7 +466,7 @@ export default function MosaicGrid({
             >
               {/* left: text panel */}
               <div
-                className="hidden md:flex w-[200px] min-w-0 shrink-0 flex-col justify-center pl-4 break-words overflow-hidden"
+                className="hidden md:flex w-[200px] min-w-0 shrink-0 flex-col justify-center pt-12 pl-4 break-words overflow-hidden"
                 onClick={(e) => e.stopPropagation()}
               >
                 {isDevMode ? (
