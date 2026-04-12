bc2a42e54fb3cf27b283eafffc4a3b68dbd90288
tomzhengy <tomzheng1012@gmail.com>
2026-04-12 03:01:21 -0700

    style: align sidebar text top with mosaic top, adjust expanded view to match

--- app/components/photography/MosaicGrid.tsx
+++ app/components/photography/MosaicGrid.tsx
@@ -256,7 +256,7 @@ export default function MosaicGrid({
       <div className="hidden md:block w-[200px] shrink-0 self-start sticky top-[8vh] h-[calc(100vh-8vh)] overflow-hidden break-words">
         {/* spacer to align with photos below header */}
         <div className="h-8 mb-8" />
-        <div className="mt-6 pt-4">
+        <div className="mt-6">
           {displayedItem && (
             <>
               {isDevMode ? (
@@ -468,7 +468,7 @@ export default function MosaicGrid({
             >
               {/* left: text panel - absolutely positioned so it doesn't affect image centering */}
               <div
-                className="hidden md:flex absolute left-4 top-0 w-[200px] min-w-0 flex-col pt-[calc(8vh+5rem)] break-words overflow-hidden z-10"
+                className="hidden md:flex absolute left-4 top-0 w-[200px] min-w-0 flex-col pt-[calc(8vh+4rem)] break-words overflow-hidden z-10"
                 onClick={(e) => e.stopPropagation()}
               >
                 {isDevMode ? (
